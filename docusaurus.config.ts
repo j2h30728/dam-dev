12a6cc3e7a2005f14dc92647f8819a9dd832f9fc
Dam <60846068+j2h30728@users.noreply.github.com>
2024-11-07 16:22:35 +0900

    build: og 교체

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/dam-dev-card.png",
     navbar: {
       title: "Dam dev",
       logo: {
